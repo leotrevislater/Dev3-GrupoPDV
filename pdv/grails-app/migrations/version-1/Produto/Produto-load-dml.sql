54ded695ba6bb3babefc73633929bc453203488a
Luiz Felipe Camboim Bender <bender@Bender-MacBook-Pro.local>
2016-11-26 19:32:37 -0200

    Adicionando sql para inserir produtos no banco

---
+++ pdv/grails-app/migrations/version-1/Produto/Produto-load-dml.sql
@@ -0,0 +1,13 @@
+--liquibase formatted sql
+--changeset jackelynoliveira:produto-load-dml
+
+INSERT INTO PRODUTO(descricao, moeda, valor, version) VALUES('Laranja', 'BRL', 1.0, 1);
+INSERT INTO PRODUTO(descricao, moeda, valor, version) VALUES('Maça', 'BRL', 1.0, 1);
+INSERT INTO PRODUTO(descricao, moeda, valor, version) VALUES('Banana', 'BRL', 1.0, 1);
+INSERT INTO PRODUTO(descricao, moeda, valor, version) VALUES('Mamão', 'BRL', 1.0, 1);
+INSERT INTO PRODUTO(descricao, moeda, valor, version) VALUES('Pêssego', 'BRL', 1.0, 1);
+INSERT INTO PRODUTO(descricao, moeda, valor, version) VALUES('Uva', 'BRL', 1.0, 1);
+INSERT INTO PRODUTO(descricao, moeda, valor, version) VALUES('Melancia', 'BRL', 1.0, 1);
+INSERT INTO PRODUTO(descricao, moeda, valor, version) VALUES('Pera', 'BRL', 1.0, 1);
+INSERT INTO PRODUTO(descricao, moeda, valor, version) VALUES('Abacaxi', 'BRL', 1.0, 1);
+INSERT INTO PRODUTO(descricao, moeda, valor, version) VALUES('Manga', 'BRL', 1.0, 1);
